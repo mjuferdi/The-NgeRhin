688dcb9673709efac66540efea81d6eee6aa3221
mjuferdi <juferdi.mario@gmail.com>
2017-01-21 15:41:54 +0100

    UpdateView, DeleteView und User Regis and Login auth erstellt

--- music/urls.py
+++ music/urls.py
@@ -6,8 +6,19 @@ app_name = 'music'
 urlpatterns = [
     # /music/
     url(r'^$', views.IndexView.as_view(), name='index'),
+
+    # /register/
+    url(r'^register/$', views.UserFormView.as_view(), name='register'),
+
     # /music/<album_id>/
     url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
+
     # /music/album/add/
     url(r'album/add/$', views.AlbumCreate.as_view(), name='album-add'),
+
+    # /music/album/<pk>/ update Album
+    url(r'album/(?P<pk>[0-9]+)/$', views.AlbumUpdate.as_view(), name='album-update'),
+
+    # /music/album/<pk>/delete/ delete Album
+    url(r'album/(?P<pk>[0-9]+)/delete/$', views.AlbumDelete.as_view(), name='album-delete'),
 ]
